95cbdc5338b3f3157644f8ac8a686e9574f3a060
Jose Paolo Talusan <josepaolo-t@mm-dhcp-126-027.naist.jp>
2018-08-29 14:02:53 +0900

    Initial Commit

---
+++ UbiHealth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UbiHealth
+//
+//  Created by Jose Paolo Talusan on 2018/08/29.
+//  Copyright © 2018 Jose Paolo Talusan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
